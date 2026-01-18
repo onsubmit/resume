d5a93b24ca4089044c6657c9d75a893567c6ce2c
Andy <onsubmit@outlook.com>
2026-01-18 14:46:14 -0800

    Add homepage

--- data/links.ts
+++ data/links.ts
@@ -1,6 +1,7 @@
 import { Hyperlink } from "../utils/hyperlink";
 
 export const links = [
+  new Hyperlink("https://anerdguy.now"),
   new Hyperlink("https://github.com/onsubmit"),
   new Hyperlink("https://www.linkedin.com/in/anerdguynow"),
 ];
