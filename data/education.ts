abf04b532b8ee3d1e3b1b30aec6c8ea68a45071d
Andy <onsubmit@outlook.com>
2026-01-19 18:17:03 -0800

    Remove dates from education

--- data/education.ts
+++ data/education.ts
@@ -1,4 +1,4 @@
 export const education: Record<string, string> = {
-  wsu: "B.S. Honors Computational Mathematics, Washington State University, 2004.",
-  microsoft: "Microsoft Test Apprentice Program, Microsoft Corporation, 2007.",
+  wsu: "B.S. Honors Computational Mathematics, Washington State University.",
+  microsoft: "Microsoft Test Apprentice Program, Microsoft Corporation.",
 };
